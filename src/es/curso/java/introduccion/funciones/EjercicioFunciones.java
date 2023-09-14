f5ae87ef8b777777cc976455408c6963ee156a7c
Alicia <alij545@gmail.com>
2023-09-14 20:44:41 +0200

    funciones again

---
+++ src/es/curso/java/introduccion/funciones/EjercicioFunciones.java
@@ -0,0 +1,43 @@
+package es.curso.java.introduccion.funciones;
+
+public class EjercicioFunciones {
+
+	public static void main(String[] args) {
+     
+		EjercicioFunciones.pintaTablaMultiplicar(1);
+		{System.out.println("");}
+		
+		String año= "1994";
+				
+		EjercicioFunciones.calculaEdad(año); {System.out.println();}
+		
+		
+	}
+
+	// int calculaEdad (String anioNacimiento)
+	public static int calculaEdad(String anioNacimiento) {
+		int anio = Integer.parseInt(anioNacimiento);
+		int edad = 2023 - anio;
+		return edad;
+	}
+
+	 // void pintaTablaMultiplicar (int numero)
+
+	public static void pintaTablaMultiplicar(int numero) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(numero + "*" + i + "=" + (numero * i));
+		}
+
+	}
+
+	// String [] juntaDatos (String texto, int numRepeticiones)
+	public static String[] juntaDatos(String texto, int numRepeticiones) {
+		 String [] datos = new String[numRepeticiones];
+		for (int i = 0; i < numRepeticiones; i++) {
+			datos[i] = texto;
+		}
+		return datos;
+
+		// #Facil, devolver el texto tantas veces como indique el numRepeticiones
+	}
+}
---
+++ src/es/curso/java/introduccion/utils/Utilidades.java
@@ -0,0 +1,8 @@
+package es.curso.java.introduccion.utils;
+
+public class Utilidades {
+      
+       
+       //+ pideDatoTexto método para pedir un String por consola a partir de un texto y que devuelva el valor seleccionado  
+
+}
