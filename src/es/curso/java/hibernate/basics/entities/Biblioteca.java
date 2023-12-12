da8d9e99cc3708cb465f637f76f7c600ef0e1481
Ajimenez1494 <alij545@gmail.com>
2023-12-12 19:29:59 +0100

    act

--- src/es/curso/java/hibernate/basics/entities/Biblioteca.java
+++ src/es/curso/java/hibernate/basics/entities/Biblioteca.java
@@ -1,9 +1,30 @@
 package es.curso.java.hibernate.basics.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="TB_BIBLIOTECAS")
 public class Biblioteca {
+	@Id
+	@SequenceGenerator(name="BIBLIOTECASGEN", sequenceName="BIBLIOTECAS_GEN", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="BIBLIOTECASGEN")
+	@Column(name="ID", nullable=false, updatable=false)
 	private long id;
 	private String nombre;
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_direccion", foreignKey = @ForeignKey(name = "fk_bibliotecadireccion") )
+  
+
 	private Direccion direccion;
 	public long getId() {
 		return id;
--- src/es/curso/java/hibernate/basics/entities/Direccion.java
+++ src/es/curso/java/hibernate/basics/entities/Direccion.java
@@ -1,6 +1,20 @@
 package es.curso.java.hibernate.basics.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_DIRECCIONES")
 public class Direccion {
+	@Id
+	@SequenceGenerator(name="DIRECCIONESGEN", sequenceName="DIRECCIONES_GEN", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="DIRECCIONESGEN")
+	@Column(name="ID", nullable=false, updatable=false)
 	private long id;
 	private String tipo;
 	private String nombre;
